7621d6858c70fe6b97e940cb1e7240cbbc249fea
Saran <s.sarannithish@gmail.com>
2026-08-12 23:50:05 +0530

    authentication issues fix

--- supabase/functions/admin-invite-user/index.ts
+++ supabase/functions/admin-invite-user/index.ts
@@ -16,7 +16,7 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "authorization, content-type"
+  "Access-Control-Allow-Headers": "authorization, content-type, x-client-info, apikey"
 };
 
 function usernameFromEmail(email: string) {
